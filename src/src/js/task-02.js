9e70e0c115f84dea005c312eabbbafcdc7572771
Kostiantyn Kuzin <worldiswonderful2012@gmail.com>
2022-08-05 14:52:07 +0300

    task2

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const elements = [];
+for (let i = 0; i < ingredients.length; i++) {
+  const option = ingredients[i];
+  const newEl = document.createElement("li");
+  newEl.textContent = option;
+  elements.push(newEl);
+}
+console.log(elements);
+const navEl = document.querySelector("#ingredients");
+navEl.append(...elements);
