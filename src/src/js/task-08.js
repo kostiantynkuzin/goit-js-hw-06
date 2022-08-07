ea164909550202de7f46724e0f36f4b401d06a91
Kostiantyn Kuzin <worldiswonderful2012@gmail.com>
2022-08-07 11:32:09 +0300

    task8

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,16 @@
+const loginFormEl = document.querySelector("form.login-form");
+const emailEl = loginFormEl.querySelector('[type="email"]');
+const passwordEl = loginFormEl.querySelector('[type="password"]');
 
+loginFormEl.addEventListener("submit", inputText);
+function inputText(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all fields");
+  }
+  console.log(`Email: ${email.value}, Password: ${password.value} `);
+  event.currentTarget.reset();
+}
