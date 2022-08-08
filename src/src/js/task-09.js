dfc96340ac738d7e43b5a209da69f020f2f3466d
Kostiantyn Kuzin <worldiswonderful2012@gmail.com>
2022-08-08 12:43:36 +0300

    task9-new

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -5,13 +5,13 @@ const bodyEl = document.querySelector("body");
 const buttonEl = document.querySelector(".change-color");
 const spanEl = document.querySelector(".color");
 
-console.log(spanEl);
+//console.log(spanEl);
 
 buttonEl.addEventListener("click", changeColor);
+const randomColor = `${getRandomHexColor()}`;
 
-function changeColor(event) {
-  event.preventDefault();
-  bodyEl.style.backgroundColor = getRandomHexColor();
+function changeColor() {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
   spanEl.innerHTML = `${getRandomHexColor()}`;
-  console.log(getRandomHexColor());
+  console.log(randomColor);
 }
