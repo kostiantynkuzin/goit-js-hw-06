10c317f806ec3bf2730c625011daf9d2e04273fb
Kostiantyn Kuzin <worldiswonderful2012@gmail.com>
2022-08-08 12:01:29 +0300

    task6-new

--- src/src/js/task-06.js
+++ src/src/js/task-06.js
@@ -3,7 +3,7 @@ const input = document.querySelector("#validation-input");
 input.addEventListener("blur", inputBlur);
 
 function inputBlur() {
-  if (+input.value.length === +input.dataset.length) {
+  if (Number(input.value.length) === Number(input.dataset.length)) {
     return input.classList.remove("invalid"), input.classList.add("valid");
   } else input.classList.remove("valid"), input.classList.add("invalid");
 }
